487b32f721e17176807c8fa09d7e33a9f7805957
cindy <cindialy@gmail.com>
2017-06-07 21:54:54 -0400

    cambio a menu2

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+var hamb= document.getElementById("menuDes");
+
+  hamb.addEventListener("click", function(){
+    
+var lista= document.getElementById("listita");
+         lista.classList.toggle("cambio") 
+
+  });
+
